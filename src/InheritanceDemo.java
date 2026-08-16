290a59b7f898b086435358b39db978515f51fe0d
yasar <saminyasarsunny@gmail.com>
2026-08-16 08:24:27 +0600

    Add files via upload

---
+++ src/InheritanceDemo.java
@@ -0,0 +1,20 @@
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        d.eat();   // inherited method
+        d.bark();
+    }
+}
